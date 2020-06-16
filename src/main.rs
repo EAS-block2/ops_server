2e6613100f4512f2b45b1262ad2d1dfd76632a9f
Jacob Ellington <jake@workstation.ellingtech.com>
2020-06-16 17:25:32 -0400

    Add bones of Revere threads

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
-use std::env;
 use std::fs;
+use std::thread;
+use std::time::Duration;
+
 fn main() {
     let points_f = "/home/jake/Documents/EAS/Block2/points.txt";
     let mut points_o = PointsStruct{points:0, buttons: 0};
     points_o.load_file(points_f);
+    spawn_revere(points_o.points, false);
+    thread::sleep(Duration::from_secs(5));
 }
+
+// Read data from a file
 struct PointsStruct {
     points: u8,
-    buttons: u8,
-}
+    buttons: u8,}
 impl PointsStruct {
     fn load_file(&mut self, file: &str) {
         let f_string = fs::read_to_string(file).expect("Something went wrong reading the file");
@@ -21,5 +26,24 @@ impl PointsStruct {
         }
         println!("points: {:?}", self.points);
         println!("buttons: {:?}", self.buttons);
+    }
+}
+//create threads to notify strobes and signs of an emergency
+fn spawn_revere(pts: u8, do_ip_fallback: bool){
+    let mut handles = vec![];
+    for i in 0..(pts+1) {
+        let handle = thread::spawn(move || {
+            let mut target = "Point".to_string();
+            if do_ip_fallback {
+                println!("Attempting IP lookup fallback");
+            }
+            else {
+                target.push_str(&i.to_string());
+                println!("Opening socket with {}", target);
+            }
+            thread::sleep(Duration::from_secs(1)); //for testing only
+            println!("Exiting Thread");
+        });
+        handles.push(handle);
     }
 }
